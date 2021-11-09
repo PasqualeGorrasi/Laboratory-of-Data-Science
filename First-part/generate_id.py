76d1b9a542fcd2ddcde9b91e57df3e8898149a4a
Pasquale Gorrasi <58590409+PasqualeGorrasi@users.noreply.github.com>
2021-11-09 13:25:12 +0100

    Add files via upload

--- First-part/generate_id.py
+++ First-part/generate_id.py
@@ -12,7 +12,7 @@ with open('C:\\Users\\pasqu\\Desktop\\Università\\Laboratory DS\\progetto\\matc
     with open('C:\\Users\\pasqu\\Desktop\\Università\\Laboratory DS\\progetto\\match_conid.csv', 'w',newline='') as out:
         
         writer = writer(out)
-        writer.writerow(['tourney_id','match_id', 'match_num','winner_id','winner_age','loser_id','score','best_of','round','minutes','w_ace','w_df','w_svpt','w_1stIn','w_1stWon','w_2ndWon','w_SvGms','w_bpSaved','w_bpFaced','l_ace','l_df','l_svpt','l_1stIn','l_1stWon','l_2ndWon','l_SvGms', 'l_bpSaved','l_bpFaced' ,'winner_rank', 'winner_rank_points' ,'loser_rank', 'loser_rank_points'])
+        writer.writerow(['tourney_id','match_id','winner_id','loser_id','score','best_of','round','minutes','w_ace','w_df','w_svpt','w_1stIn','w_1stWon','w_2ndWon','w_SvGms','w_bpSaved','w_bpFaced','l_ace','l_df','l_svpt','l_1stIn','l_1stWon','l_2ndWon','l_SvGms', 'l_bpSaved','l_bpFaced' ,'winner_rank', 'winner_rank_points' ,'loser_rank', 'loser_rank_points'])
         next(f)        
         reader = reader(f)
         
